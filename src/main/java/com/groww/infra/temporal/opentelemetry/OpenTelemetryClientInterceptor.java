c753cd2cf6c1d588cf9fdb3bbf7a6899dca9589a
JayanthTatina <tatina.jayanth@groww.in>
2024-06-27 16:52:07 +0530

    cleint interceptor

--- src/main/java/com/groww/infra/temporal/opentelemetry/OpenTelemetryClientInterceptor.java
+++ src/main/java/com/groww/infra/temporal/opentelemetry/OpenTelemetryClientInterceptor.java
@@ -1,7 +1,30 @@
 package com.groww.infra.temporal.opentelemetry;
 
+import com.groww.infra.temporal.opentelemetry.internal.ContextAccessor;
+import com.groww.infra.temporal.opentelemetry.internal.OpenTelemetryWorkflowClientCallsInterceptor;
+import com.groww.infra.temporal.opentelemetry.internal.SpanFactory;
+import io.temporal.common.interceptors.WorkflowClientCallsInterceptor;
 import io.temporal.common.interceptors.WorkflowClientInterceptorBase;
 
 public class OpenTelemetryClientInterceptor extends WorkflowClientInterceptorBase {
+  private final OpenTelemetryOptions options;
+  private final SpanFactory spanFactory;
+  private final ContextAccessor contextAccessor;
 
+  public OpenTelemetryClientInterceptor() {
+    this(OpenTelemetryOptions.getDefaultInstance());
+  }
+
+  public OpenTelemetryClientInterceptor(OpenTelemetryOptions options) {
+    this.options = options;
+    this.spanFactory = new SpanFactory(options);
+    this.contextAccessor = new ContextAccessor(options);
+  }
+
+  @Override
+  public WorkflowClientCallsInterceptor workflowClientCallsInterceptor(
+      WorkflowClientCallsInterceptor next) {
+    return new OpenTelemetryWorkflowClientCallsInterceptor(
+        next, options, spanFactory, contextAccessor);
+  }
 }
